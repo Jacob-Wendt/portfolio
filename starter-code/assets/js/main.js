2a570d7e96cc1b629582186d454a8539ca4e277d
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-04-22 11:50:54 -0500

    got the dang video workin

--- starter-code/assets/js/main.js
+++ starter-code/assets/js/main.js
@@ -3,7 +3,33 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+function scaleToFill() {
+    $('video.video-background').each(function(index, videoTag) {
+       var $video = $(videoTag),
+           videoRatio = videoTag.videoWidth / videoTag.videoHeight,
+           tagRatio = $video.width() / $video.height(),
+           val;
 
+       if (videoRatio < tagRatio) {
+           val = tagRatio / videoRatio * 1.02;
+       } else if (tagRatio < videoRatio) {
+           val = videoRatio / tagRatio * 1.02;
+       }
+
+       $video.css('transform','scale(' + val  + ',' + val + ')');
+
+    });    
+}
+
+$(function () {
+    scaleToFill();
+
+    $('.video-background').on('loadeddata', scaleToFill);
+
+    $(window).resize(function() {
+        scaleToFill();
+    });
+});
 (function($) {
 
 	var	$window = $(window),
